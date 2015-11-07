ae2495ffcd1c5e7d371298223c815538ab7963ce
xavisanchezmir <xavi.sanchez.mir@gmail.com>
2015-11-07 08:01:42 +0100

    Adding Charts

--- models/registres_model.php
+++ models/registres_model.php
@@ -7,7 +7,7 @@
  */
 class Registres_Model extends Model {
 
-    function __contstruct () {
+    function __construct () {
         parent::__construct();
         $this->table = "registres";
     }
